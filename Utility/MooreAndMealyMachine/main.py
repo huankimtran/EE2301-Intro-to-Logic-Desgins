72a7e7069657f27358f324d05e48ea2a9b79b580
HuanKTran <huankimtran@gmail.com>
2019-11-26 05:36:35 -0600

    Begin separating JKFF and DFF implementation

--- Utility/MooreAndMealyMachine/main.py
+++ Utility/MooreAndMealyMachine/main.py
@@ -1,4 +1,5 @@
-from StateDiagram import *
+import MealyStateDiagram as MeDFF
+import MealyStateDiagramJKFF as MeJKFF
 
 # File to parse here
 stateDiagramDescriptionFile='./test.txt'
@@ -8,7 +9,11 @@ stateDiagramDescriptionFile='./test.txt'
 f=open(stateDiagramDescriptionFile,'r')
 line=f.readline()
 if line[:-1]=='Me':
-	MealyMachine=MealyStateDiagram(stateDiagramDescriptionFile)
+	DorJK=input('Do you want JKFF or DFF implementation? (DFF/JKFF): ')
+	if DorJK=='DFF':
+		Machine=MeDFF.MealyStateDiagram(stateDiagramDescriptionFile)
+	else:
+		Machine=MeJKFF.MealyStateDiagram(stateDiagramDescriptionFile)
 else:
 	# Complete the Moore!!
 	pass
